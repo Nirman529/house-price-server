787494d9851127c1f0e5450a5c981591c8ebf802
Nirman529 <68377229+Nirman529@users.noreply.github.com>
2022-09-01 15:55:20 +0530

    working in single file

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, jsonify, request, make_response, url_for, redirect
-import util
+import numpy as np
+import json
+from flask import Flask, render_template, jsonify, request
+# import util
 import pickle
 
 app = Flask(__name__)
@@ -8,51 +10,96 @@ app = Flask(__name__)
 model = pickle.load(open('artifacts/house_price_predict_model.pickle', 'rb'))
 
 
+__location = None
+__data_columns = None
+__model = None
+
+
+def get_estimated_price(location, total_sqft, bhk, bath, balcony):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = bath
+    x[1] = balcony
+    x[2] = bhk
+    x[3] = total_sqft
+    if(loc_index >= 0):
+        x[loc_index] = 1
+
+    # print("value : ", round(__model.predict([x])[0], 2))
+    return round(__model.predict([x])[0], 2)
+
+
+def get_location_names():
+    # # print(__location)
+    return __location
+
+
+def load_saved_artifacts():
+    # print('loading saved artifacts')
+    global __data_columns
+    global __location
+    global __model
+
+    with open('artifacts\\columns.json', 'r') as f:
+        # print('loading data columns and location.')
+        __data_columns = json.load(f)['data_columns']
+        __location = __data_columns[3:]
+
+    with open('artifacts\\house_price_predict_model.pickle', 'rb') as f:
+        __model = pickle.load(f)
+
+
 @app.route('/')
 def home():
-    response_loc = util.get_location_names()
+    response_loc = get_location_names()
     # location = jsonify(response)'
-    # print("Loc", json.dumps(get_location_names()))
+    # # print("Loc", json.dumps(get_location_names()))
     # response = ["abc", "def"]
     return render_template('index.html', response_loc=response_loc)
     # return render_template('student.html')
 
 
-@app.route('/get_location_names')
-def get_location_names():
-    response = jsonify({
-        'locations': util.get_location_names()
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    print("res : ", util.get_location_names())
-    return response
+# @app.route('/get_location_names')
+# def get_location_names():
+#     response = jsonify({
+#         'locations': get_location_names()
+#     })
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     # print("res : ", get_location_names())
+#     return response
 
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
-    print("home")
+    # print("home")
     total_sqft = float(request.form['SquareFt'])
     location = request.form['location']
     bhk = int(request.form['uiBHK'])
     bath = int(request.form['uiBath'])
     balcony = int(request.form['uiBalcony'])
-    print(total_sqft, location, bhk, bath, balcony)
-    response = util.get_estimated_price(
+    # print(total_sqft, location, bhk, bath, balcony)
+    response = get_estimated_price(
         location, total_sqft, bhk, bath, balcony)
     # response = jsonify({
     #     'estimated_price': util.get_estimated_price(total_sqft)
     # })
 
     # response.headers.add('Access-Control-Allow-Origin', '*')
-    print(response)
+    # print(response)
 
     # output = request.form.to_dict()
     # result = output['result']
-    response_loc = util.get_location_names()
+    response_loc = get_location_names()
     return render_template('index.html', result=response, response_loc=response_loc)
 
 
 if __name__ == '__main__':
-    print("Starting flask server.")
-    util.load_saved_artifacts()
+    # print("Starting flask server.")
+    load_saved_artifacts()
+
+    # util.load_saved_artifacts()
     app.run()
